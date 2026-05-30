67b6ffe3f1d1ff69aa406e18463e5aaa8c3d5eb7
Hritwik86 <hritwikmahat086@gmail.com>
2026-05-30 10:31:18 +0530

    completed Stage 2 and Frontend Setup and readme files updated

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1>Hi This is Hritwik Mahato</h1>
+    <div>This is just Setup Babes.</div>
+    <h2 className='text-5xl font-bold text-center text-blue-500'>Hritwik</h2>
+    <h1 className="text-5xl font-bold text-red-500">
+      Tailwind Test
+    </h1>
+    </>
+    
+
+  )
+}
+
+export default App
